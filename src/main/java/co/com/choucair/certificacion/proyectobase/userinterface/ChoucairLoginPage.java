6ef701e238a1892c85ee17237ea072070ff1a952
robertl2019 <robertogoleo11@gmail.com>
2021-03-15 21:48:19 -0500

    correccion de errores y gitignore

--- src/main/java/co/com/choucair/certificacion/proyectobase/userinterface/ChoucairLoginPage.java
+++ src/main/java/co/com/choucair/certificacion/proyectobase/userinterface/ChoucairLoginPage.java
@@ -5,7 +5,7 @@ import net.serenitybdd.screenplay.targets.Target;
 import org.openqa.selenium.By;
 
 
-public class ChoucairLoginPage {
+public class ChoucairLoginPage{
     public static final Target LOGIN_BUTTON = Target.the("button that shows us the form to login ") .
             located(By.xpath("//div[@class='d-none d-lg-block']//strong[contains(text(),'Ingresar')]"));
     public static final Target INPUT_USER = Target.the("field where the user name is entered").located(By.id("username"));
